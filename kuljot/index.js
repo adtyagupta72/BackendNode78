463f7d4fa037b5564329050d381e085227f283d7
Kuljot Singh <kuljotsingh0511@gmail.com>
2026-03-10 21:45:53 +0530

    node js npm package complete

---
+++ kuljot/index.js
@@ -0,0 +1,16 @@
+// console.log("Hello there!");
+// console.log(`V8 version ${process.versions.v8}`);
+
+// console.log("first");
+// setTimeout(() => console.log("Third"),0);
+// Promise.resolve().then(() => console.log("second"))
+// console.log("Fourth")
+
+
+console.log("1.start")
+process.nextTick(() => console.log("2.Next tick"))
+
+Promise.resolve().then(() => console.log("3. Promise"))
+setTimeout(() => console.log("4.Timeout"),0)
+setImmediate(() => console.log("5.immediate"));
+console.log("6. End");
