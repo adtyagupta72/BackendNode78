292e73286e6138f2d7997b6c074c2aa423f498e8
Aditya Gupta <adtyagupta72@gmail.com>
2026-03-10 19:52:44 +0530

    Node Event Loop basics

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello there!")
+// console.log("Hello there!")
+// console.log(`V8 version: ${process.versions.v8}`);
+console.log('First');
+setTimeout(() => console.log('Third'), 0);
+Promise.resolve().then(() => console.log('Second'));
+console.log('Fourth');
