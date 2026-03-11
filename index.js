ae970fadc09d0630c263389109e5c87d9d68362e
Aditya Gupta <adtyagupta72@gmail.com>
2026-03-11 19:57:58 +0530

    NodeJS: Synchronous and Async dir read

--- index.js
+++ index.js
@@ -4,13 +4,24 @@
 // setTimeout(() => console.log('Third'), 0);
 // Promise.resolve().then(() => console.log('Second'));
 // console.log('Fourth');
-console.log('1. Start');
-// Next tick queue
-process.nextTick(() => console.log('2. Next tick'));
-// Microtask queue (Promise)
-Promise.resolve().then(() => console.log('3. Promise'));
-// Timer phase
-setTimeout(() => console.log('4. Timeout'), 0);
-// Check phase
-setImmediate(() => console.log('5. Immediate'));
-console.log('6. End');
+// console.log('1. Start');
+// // Next tick queue
+// process.nextTick(() => console.log('2. Next tick'));
+// // Microtask queue (Promise)
+// Promise.resolve().then(() => console.log('3. Promise'));
+// // Timer phase
+// setTimeout(() => console.log('4. Timeout'), 0);
+// // Check phase
+// setImmediate(() => console.log('5. Immediate'));
+// console.log('6. End');
+
+fs = require('fs');
+function getData(err, data)
+{
+   console.log('data:', data)
+   console.log('err:', err)
+}
+//fs.readdir('/Users/adtyagupta/Documents/', getData);
+fs.readdir('/', getData);
+console.log("This is Asynchronous!!")
+
