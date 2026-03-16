965cb5b9d36071272ae7fdd0a825d0d9c7cad38e
Kuljot Singh Rajpal <uraan.kuljot@gmail.com>
2026-03-16 22:53:48 +0530

    read file sync in fs module.

--- kuljot/index.js
+++ kuljot/index.js
@@ -7,19 +7,78 @@
 // console.log("Fourth")
 
 
-console.log("1.start")
-process.nextTick(() => console.log("2.Next tick"))
+// console.log("1.start")
+// process.nextTick(() => console.log("2.Next tick"))
 
-Promise.resolve().then(() => console.log("3. Promise"))
-setTimeout(() => console.log("4.Timeout"),0)
-setImmediate(() => console.log("5.immediate"));
-console.log("6. End");
+// Promise.resolve().then(() => console.log("3. Promise"))
+// setTimeout(() => console.log("4.Timeout"),0)
+// setImmediate(() => console.log("5.immediate"));
+// console.log("6. End");
 
-fs = require("fs");
-function getData(err,data) {
-    console.log("data :",data)
-    console.log("err :",err)
-}
+// fs = require("fs");
+// function getData(err,data) {
+//     console.log("data :",data)
+//     console.log("err :",err)
+// }
 
-fs.readdir('H:/Windows C/DESKTOP/Javascript learn/React learn/my-first-router/',getData);
-console.log("This is asyncronous calls");
+// fs.readdir('H:/Windows C/DESKTOP/Javascript learn/React learn/my-first-router/',getData);
+// console.log("This is asyncronous calls");
+
+
+// const EventEmitter = require ("events");
+// class MyEmitter extends EventEmitter{}
+
+// const myEmitter = new MyEmitter();
+// myEmitter.on('parcelRecieved', () => {
+//     console.log("Parcel recieved event occured");
+
+// })
+// myEmitter.emit('parcelRecieved')
+
+// const EventEmitter = require ("events");
+// class MyEmitter extends EventEmitter{}
+
+// const myEmitter = new MyEmitter();
+// myEmitter.on('parcelRecieved', () => {
+//     console.log("Parcel recieved event occured");
+
+// })
+// // myEmitter.emit('parcelRecieved')
+
+// let eventCallBack = () => {
+//     console.log("Parcel recieved event occured");
+// }
+
+// myEmitter.emit('parcelRecieved',eventCallBack)
+// myEmitter.on('parcelRecieved')
+
+
+
+// const fs = require("fs");
+// console.log("1.starting file read");
+
+// try {
+//     const data = fs.readFileSync('myFile.txt','utf-8');
+//     console.log("2.File content :",data);
+    
+// } catch (error) {
+//     console.log("Error",error);
+
+    
+// }
+// console.log("3.Done the reading data");
+
+
+
+// asyncronous call 
+
+const fs = require("fs")
+
+console.log("1.starting file read");
+
+    fs.readFile('myFile.txt','utf-8',(err,data) => {
+        if(err)
+            throw err;
+        console.log("2. File content data",data)
+    });
+console.log("3.Done the reading data");
