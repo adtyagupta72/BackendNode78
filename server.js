f5248e213d516e127b1385a6c4faa4597fd087a6
Adtya Gupta <adtyagupta72@gmail.com>
2026-03-20 08:29:54 +0530

    Node: Completed with basics

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 const http = require('http');
 const server = http.createServer((request, response)=>
 {
-   response.writeHead(200, {'Content-Type':'Text/plain'});
-   response.end("Hello world, This server is now running!!!");
+   console.log("URL:", request.url)
+   switch(request.url)
+   {
+      case "/abc": 
+      {
+         response.writeHead(200, {'Content-Type':'Text/plain'});
+         response.end("Here is your abc: ABC");
+         console.log("Here is your abc: ABC")
+         break;
+      }
+      case "/def": 
+      {
+         response.writeHead(200, {'Content-Type':'Text/plain'});
+         response.end("DEF!");
+         console.log("DEF!")
+         break
+      }
+      case "/xyz": 
+      {
+         response.writeHead(200, {'Content-Type':'Text/plain'});
+         response.end("This is X with Y and Zee!");    
+         console.log("This is X with Y and Zee!")
+         break
+      }
+      default:
+      {
+         response.writeHead(200, {'Content-Type':'Text/plain'});
+         response.end("404, Page not found!");
+      }
+   }
+   
 })
 console.log("port 3000")
 server.listen(3000, '127.0.0.1');
